384b93c926d7c6b5e40a45539b909fbfe5af7766
NCR-2005 <nikhilcrpeddinti@gmail.com>
2025-10-26 16:18:05 +0530

    created Appointment entity in AppointmentService

--- src/main/java/com/flmhospitals/model/Appointment.java
+++ src/main/java/com/flmhospitals/model/Appointment.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -20,6 +22,7 @@ import lombok.NoArgsConstructor;
 public class Appointment {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private String appointmentId;
 	
 	private Long patientId;
